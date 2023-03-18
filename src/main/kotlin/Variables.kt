5127618d62cabc3a745e0c05eb0b5a3f805e559d
Renan1fps <renan.sergipe68@gmail.com>
2023-03-18 15:08:13 -0300

    feat: variables

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    // types mutable variables
+    var name: String = "Hello my name is Renan"
+    name = "Hello my name is Sergio"
+    println(name)
+
+    // const variables
+    val name2: String = "hello"
+    println(name2)
+}
